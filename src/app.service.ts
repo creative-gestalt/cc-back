83762dbc684f4cb959b8ef24db51cfb07f4e399d
creative-gestalt <lordweasel6@gmail.com>
2022-01-03 14:11:41 -0700

    fix UGHasdfasdfa sdfasdfasd

--- src/app.service.ts
+++ src/app.service.ts
@@ -73,8 +73,8 @@ export class AppService {
     );
     await exec(`sudo systemctl enable ${service.name}.service`);
     await exec(`git -C ~/${service.workdir}/ pull`);
-    await exec(`npm i --prefix=/home/nick/${service.workdir}/`);
-    await exec(`npm run build --prefix=/home/nick/${service.workdir}/`);
+    // await exec(`npm i --prefix=/home/nick/${service.workdir}/`);
+    // await exec(`npm run build --prefix=/home/nick/${service.workdir}/`);
     await exec(`sudo service ${service.name} start`);
 
     return 'Service Created';
