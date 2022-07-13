0e798ae3f24a5f5dae4157bc2c3ed0b9c00a0866
thegestalt <lordweasel6@gmail.com>
2022-07-13 10:45:34 -0600

    fixed cd and added image prune.

--- src/app.service.ts
+++ src/app.service.ts
@@ -24,7 +24,7 @@ export class AppService {
 
   async buildProject(projectName: string): Promise<boolean> {
     return await exec(
-      `cd ~/Projects/${projectName} docker compose down && git pull && docker compose build --force-rm && docker compose up -d`,
+      `cd ~/Projects/${projectName} && docker compose down && git pull && docker compose build --force-rm && docker compose up -d && docker image prune -f`,
       { shell: '/bin/bash' },
     )
       .then(() => true)
